5b56ecf193e126059cc1e0895d9ba499c68820b3
teresinaho <44671787+teresinaho@users.noreply.github.com>
2019-05-20 23:04:33 +0800

    tele prefab reposition

--- Assets/Prototype/Scripts/FollowTransform.cs
+++ Assets/Prototype/Scripts/FollowTransform.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class FollowTransform : MonoBehaviour
 {
     public Transform target;
+    public Vector3 offset;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +16,6 @@ public class FollowTransform : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.position = target.position;
+        transform.position = target.position+offset;
     }
 }

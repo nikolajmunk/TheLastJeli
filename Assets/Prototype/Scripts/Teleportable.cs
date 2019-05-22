422d69b242370936a8b1d913ca21d4357e63e364
nikolajmunk <28557236+nikolajmunk@users.noreply.github.com>
2019-05-22 17:45:30 +0200

    Experiments in coloring teleport effects.

--- Assets/Prototype/Scripts/Teleportable.cs
+++ Assets/Prototype/Scripts/Teleportable.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Teleportable : MonoBehaviour
 {
     public bool canBeTeleported = false;
+    public Color beamColor0;
+    public Color beamColor1;
+    public Gradient particlesGradient;
     
     // Start is called before the first frame update
     void Start()
--- Assets/Prototype/Scripts/Teleportation.cs
+++ Assets/Prototype/Scripts/Teleportation.cs
@@ -9,8 +9,17 @@ public class Teleportation : MonoBehaviour
     public LineRenderer lr;
     public GameObject[] particles1;
     public GameObject[] particles2;
+    public Teleportable actor1Teleportable;
+    public Teleportable actor2Teleportable;
 
     public Animator anim;
+
+    private Material m_instancedMaterial;
+    private Material InstancedMaterial
+    {
+        get { return m_instancedMaterial; }
+        set { m_instancedMaterial = value; }
+    }
 
     public void SetCanBeTeleported(int canBeTeleported)
     {
@@ -47,12 +56,33 @@ public class Teleportation : MonoBehaviour
             obj.GetComponent<FollowTransform>().target = target;
         }
     }
-    
+
+    private void AssignParticleValues(GameObject[] objects, Teleportable actorTeleportable)
+    {
+        ParticleSystem ps;
+        foreach (GameObject obj in objects)
+        {
+            Debug.Log("assigned.");
+            ps = obj.GetComponent<ParticleSystem>();
+            ParticleSystem.MainModule psMain = ps.main;
+            psMain.startColor = actorTeleportable.particlesGradient;
+        }
+    }
+
     public void Initialize() {
         lr.GetComponent<RenderLine>().point1 = actor1;
         lr.GetComponent<RenderLine>().point2 = actor2;
+
+        actor1Teleportable = actor1.GetComponent<Teleportable>();
+        actor2Teleportable = actor2.GetComponent<Teleportable>();
+
+        InstancedMaterial = lr.material;
+        InstancedMaterial.SetColor("_Color0", actor1Teleportable.beamColor0);
+        lr.material = InstancedMaterial;
         AssignFollowTarget(particles1, actor1);
         AssignFollowTarget(particles2, actor2);
+        AssignParticleValues(particles1, actor1Teleportable);
+        AssignParticleValues(particles2, actor1Teleportable);
 
     }
 

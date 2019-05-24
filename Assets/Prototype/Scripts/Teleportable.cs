f3f7250ffab73570b2e5081315b92ecec955b9e1
nikolajmunk <28557236+nikolajmunk@users.noreply.github.com>
2019-05-24 11:06:27 +0200

    revert2

--- Assets/Prototype/Scripts/Teleportable.cs
+++ Assets/Prototype/Scripts/Teleportable.cs
@@ -5,10 +5,19 @@ using UnityEngine;
 public class Teleportable : MonoBehaviour
 {
     public bool canBeTeleported = false;
+<<<<<<< HEAD
+=======
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 1887384... Merge branch 'master' of https://github.com/nikolajmunk/TheLastJeli
     public Color beamColor0;
     public Color beamColor1;
     public Gradient particlesGradient;
     public bool isCow = false;
+<<<<<<< HEAD
+=======
+>>>>>>> 1301bd727bb7bb346367bb18e2bc4f319146e3b2
+>>>>>>> parent of 1887384... Merge branch 'master' of https://github.com/nikolajmunk/TheLastJeli
     
     // Start is called before the first frame update
     void Start()

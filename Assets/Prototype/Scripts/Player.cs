b92f9de15b8de945d9ade0dab390303677dddb8e
nikolajmunk <28557236+nikolajmunk@users.noreply.github.com>
2019-05-31 23:11:09 +0200

    Commented out most unneeded stuff from Player
    We might delete the commented stuff later. This is in preparation for adding in dead players, as they will also need Player components but do not require most of the stuff in the component.

--- Assets/Prototype/Scripts/Player.cs
+++ Assets/Prototype/Scripts/Player.cs
@@ -10,103 +10,103 @@ using System.Collections;
 /// <summary>
 /// Player script. Manages the health and interaction with enemies of the player.
 /// </summary>
-[RequireComponent (typeof(PlatformerController))]
+//[RequireComponent (typeof(PlatformerController))]
 public class Player : MonoBehaviour
 {
     public string playerName;
     public PlayerActions Actions;
-    public enum PlayerStatus
-	{
-		Hurt,
-		Active,
-		InActive,
-		Dead
-	}
+ //   public enum PlayerStatus
+	//{
+	//	Hurt,
+	//	Active,
+	//	InActive,
+	//	Dead
+	//}
 
-	[Tooltip ("Number of lifes of the player.")]
-	[SerializeField] int hitPoints = 8;
-	[Tooltip ("Duration of the blinking and stunning when hurt by an enemy.")]
-	[SerializeField] float hurtTimer = 0.1f;
-	[Tooltip ("Object to be spawned on death.")]
-	[SerializeField] GameObject deadPrefab = null;
+	//[Tooltip ("Number of lifes of the player.")]
+	//[SerializeField] int hitPoints = 8;
+	//[Tooltip ("Duration of the blinking and stunning when hurt by an enemy.")]
+	//[SerializeField] float hurtTimer = 0.1f;
+	//[Tooltip ("Object to be spawned on death.")]
+	//[SerializeField] GameObject deadPrefab = null;
 
-	PlatformerController controller;
-	SpriteRenderer[] sr;
-	PlayerStatus status;
-	Coroutine hurtRoutine;
+	//PlatformerController controller;
+	//SpriteRenderer[] sr;
+	//PlayerStatus status;
+	//Coroutine hurtRoutine;
 
 
     //public Transform spawnedAt;
 
 	void Awake ()
 	{
-		controller = GetComponent<PlatformerController> ();
-		sr = GetComponentsInChildren<SpriteRenderer> ();
-		status = PlayerStatus.Active;
+		//controller = GetComponent<PlatformerController> ();
+		//sr = GetComponentsInChildren<SpriteRenderer> ();
+		//status = PlayerStatus.Active;
 	}
 
 	/// <summary>
 	/// Makes the player jump upwards by force.
 	/// </summary>
 	/// <param name="force">Strength of upwards push.</param>
-	public void ForceJump (float force)
-	{
-		controller.ForceJump (force);
-	}
+	//public void ForceJump (float force)
+	//{
+	//	controller.ForceJump (force);
+	//}
 
 	/// <summary>
 	/// Hurt the Player. The player will lose one hitpoint and is invulnerable for hurtTimer time.
 	/// </summary>
-	public void Hurt ()
-	{
-		if (status != PlayerStatus.Active) {
-			return;
-		}
+	//public void Hurt ()
+	//{
+	//	if (status != PlayerStatus.Active) {
+	//		return;
+	//	}
 
-		hitPoints--;
-		UIManager.SetLifes (hitPoints);
-		if (hitPoints <= 0) {
-			Die ();
-			return;
-		}
-		if (hurtRoutine != null) {
-			StopCoroutine (hurtRoutine);
-		}
-		hurtRoutine = StartCoroutine (HurtRoutine ());
-	}
+	//	hitPoints--;
+	//	UIManager.SetLifes (hitPoints);
+	//	if (hitPoints <= 0) {
+	//		Die ();
+	//		return;
+	//	}
+	//	if (hurtRoutine != null) {
+	//		StopCoroutine (hurtRoutine);
+	//	}
+	//	hurtRoutine = StartCoroutine (HurtRoutine ());
+	//}
 
-	IEnumerator HurtRoutine ()
-	{
-		status = PlayerStatus.Hurt;
-		float timer = 0;
-		bool blink = false;
-		while (timer < hurtTimer) {
-			blink = !blink;
-			timer += Time.deltaTime;
-			if (blink) {
-				foreach (SpriteRenderer rend in sr) {
-					rend.color = Color.white;
-				}
-			} else {
-				foreach (SpriteRenderer rend in sr) {
-					rend.color = Color.red;
-				}
-			}
-			yield return new WaitForSeconds (0.05f);
-		}
-		foreach (SpriteRenderer rend in sr) {
-			rend.color = Color.white;
-		}
-		status = PlayerStatus.Active;
-	}
+	//IEnumerator HurtRoutine ()
+	//{
+	//	status = PlayerStatus.Hurt;
+	//	float timer = 0;
+	//	bool blink = false;
+	//	while (timer < hurtTimer) {
+	//		blink = !blink;
+	//		timer += Time.deltaTime;
+	//		if (blink) {
+	//			foreach (SpriteRenderer rend in sr) {
+	//				rend.color = Color.white;
+	//			}
+	//		} else {
+	//			foreach (SpriteRenderer rend in sr) {
+	//				rend.color = Color.red;
+	//			}
+	//		}
+	//		yield return new WaitForSeconds (0.05f);
+	//	}
+	//	foreach (SpriteRenderer rend in sr) {
+	//		rend.color = Color.white;
+	//	}
+	//	status = PlayerStatus.Active;
+	//}
 
 	/// <summary>
 	/// Destroy the player and spawn the death animation.
 	/// </summary>
-	public void Die ()
-	{
-		Instantiate<GameObject> (deadPrefab, transform.position, transform.rotation);
-		Destroy (gameObject);
-	}
+	//public void Die ()
+	//{
+	//	Instantiate<GameObject> (deadPrefab, transform.position, transform.rotation);
+	//	Destroy (gameObject);
+	//}
 }
 

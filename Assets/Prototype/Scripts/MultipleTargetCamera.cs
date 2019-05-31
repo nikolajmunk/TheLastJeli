a817864cf1ce261bf5b14931c5776211a272e388
nikolajmunk <28557236+nikolajmunk@users.noreply.github.com>
2019-05-31 23:12:45 +0200

    New camera script for sexier player follow.
    Currently inactive.

---
+++ Assets/Prototype/Scripts/MultipleTargetCamera.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultipleTargetCamera : MonoBehaviour
+{
+    Vector3 offset;
+    Vector3 velocity;
+    public float smoothTime = .5f;
+    List<Transform> targets;
+
+    private void Awake()
+    {
+        targets = GameManager.instance.playerPositions;
+        offset = transform.position - targets[0].position;
+    }
+
+    void LateUpdate()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, GetTargetPosition(), ref velocity, smoothTime);
+    }
+
+    Vector3 GetTargetPosition()
+    {
+        var frontmostPosition = targets[0].position;
+        var pos = new Vector3(frontmostPosition.x, GetCentroid().y, frontmostPosition.z) + offset;
+        return pos;
+    }
+
+    Bounds GetEncapsulatingBounds()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 1; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+        
+
+        return bounds;
+    }
+
+    Vector3 GetCentroid()
+    {
+        return GetEncapsulatingBounds().center;
+    }
+
+}

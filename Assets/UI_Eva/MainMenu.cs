d89c487ff9b8336d054bf262e4791ea9152637d1
Joakim Carlsen <joakim.carlsen@hotmail.com>
2019-06-05 14:12:10 +0200

    More cow in the menu ;)

--- Assets/UI_Eva/MainMenu.cs
+++ Assets/UI_Eva/MainMenu.cs
@@ -6,6 +6,11 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     PlayerManager playerManager;
+    public ParticleSystem cowsteroids;
+    private ParticleSystem.MainModule cowMain;
+    private ParticleSystem.EmissionModule cowEmission;
+    private int newMax = 2;
+    private AudioSource moo;
 
     private void Start()
     {
@@ -14,26 +19,47 @@ public class MainMenu : MonoBehaviour
         playerManager.acceptNewPlayers = false;
         GameManager.instance.GetComponent<AudioSource>().clip = GameManager.instance.GetComponent<AudioHandler>().GetAudioClipByName("MenuMusic");
         GameManager.instance.GetComponent<AudioSource>().Play();
+
+        cowEmission = cowsteroids.emission;
+        cowMain = cowsteroids.main;
+
+        moo = GetComponent<AudioSource>();
+
     }
 
     // Start is called before the first frame update
     public void ToLobby()
     {
         SceneManager.LoadScene("Lobby_master");
-        
+
     }
     public void Quit()
     {
         Application.Quit();
-        #if UNITY_EDITOR
+#if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
-        #endif
-    } 
-    
+#endif
+    }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    public void MoreCows()
+    {
+        newMax += 1;
+
+        if(!moo.isPlaying)
+            moo.Play();
+
+        if(cowMain.maxParticles <= 1500)
+            cowMain.maxParticles += 50;
+
+        if(newMax <= 10)
+            cowEmission.rateOverTime = newMax;
+
     }
 }
